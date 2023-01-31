8b3bc36abc291b2a61cce81ba31342c1ff902cbb
ziya_312 <manjak312@gmail.com>
2023-02-01 04:28:57 +0900

    초기 구현

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { RecoilRoot } from "recoil";
+import { AddMembers } from "./components/AddMembers";
+import { CreateGroup } from "./components/CreateGroup";
+import { ExpenseMain } from "./components/ExpenseMain";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <RecoilRoot>
+        <Routes>
+          <Route path="/" element={<CreateGroup />} />
+          <Route path="/members" element={<AddMembers />} />
+          <Route path="/expense" element={<ExpenseMain />} />
+        </Routes>
+      </RecoilRoot>
+    </BrowserRouter>
   );
 }
 
