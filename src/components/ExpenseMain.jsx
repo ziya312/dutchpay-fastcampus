56e544ae51ec9a18eeb92301278af53afa61e6cb
ziya_312 <manjak312@gmail.com>
2023-02-04 04:02:00 +0900

    정산 컴포넌트

--- src/components/ExpenseMain.jsx
+++ src/components/ExpenseMain.jsx
@@ -1,17 +1,61 @@
+import { Container, Row, Col } from "react-bootstrap";
+import { AddExpenseForm } from "./AddExpenseForm";
+import { ExpenseTable } from "./ExpenseTable";
+import styled from "styled-components";
+import { useRecoilValue } from "recoil";
+import { groupNameState } from "../state/groupName";
+
 export const ExpenseMain = () => {
   return (
-    <div>
-      ExpenseMain
-      {/* Left Pane */}
-      <div>
-        {/* TODO : 비용 추가 폼 컴포넌트 렌더링 */}
-        {/* TODO : 정산 결과 컴포넌트 렌더링 */}
-      </div>
-      {/* Right Pane */}
-      <div>
-        {/* TODO : 그룹명 헤더 렌더링 */}
-        {/* TODO : 비용 리스트 컴포넌트 렌더링 */}
-      </div>
-    </div>
+    <Container fluid>
+      <Row>
+        <Col xs={12} sm={5} md={4}>
+          <LeftPane />
+        </Col>
+        <Col>
+          <RightPane />
+        </Col>
+      </Row>
+    </Container>
   );
 };
+
+const LeftPane = () => {
+  return (
+    <Container>
+      <AddExpenseForm />
+      {/* TODO : 정산 결과 컴포넌트 렌더링 */}
+    </Container>
+  );
+};
+
+const RightPane = () => {
+  const groupName = useRecoilValue(groupNameState);
+
+  return (
+    <StyledContainer>
+      <Row>
+        <StyledGroupName>{groupName || "그룹 이름"}</StyledGroupName>
+      </Row>
+      <Row>
+        <ExpenseTable />
+      </Row>
+    </StyledContainer>
+  );
+};
+
+const StyledContainer = styled(Container)`
+  padding: 100px 32px 100px 32px;
+`;
+
+const StyledGroupName = styled.h2`
+  margin-bottom: 80px;
+  font-size: 48px;
+  line-height: 48px;
+  font-weight: 700;
+  text-align: center;
+`;
+
+const StyledThead = styled.thead`
+  color: #ff7a00;
+`;
---
+++ src/components/ExpenseMain.spec.jsx
@@ -0,0 +1,130 @@
+import { render, screen, within } from "@testing-library/react";
+import { RecoilRoot } from "recoil";
+import { ExpenseMain } from "./ExpenseMain";
+import userEvent from "@testing-library/user-event";
+import { groupMembersState } from "../state/groupMembers";
+
+const renderComponent = () => {
+  render(
+    <RecoilRoot
+      initializeState={(snap) => {
+        snap.set(groupMembersState, ["영수", "영희"]);
+      }}
+    >
+      <ExpenseMain />
+    </RecoilRoot>
+  );
+
+  const dateInput = screen.getByPlaceholderText(/결제 날짜/i);
+  const descInput = screen.getByPlaceholderText(/비용 내역/i);
+  const amountInput = screen.getByPlaceholderText(/금액/i);
+  const payerInput = screen.getByDisplayValue(/누가 결제/i);
+  const addButton = screen.getByText("추가하기");
+
+  const descErrorMessage = screen.getByText(
+    "비용에 대한 설명을 입력해 주세요."
+  );
+  const payerErrorMessage = screen.getByText("결제자를 선택해 주세요.");
+  const amountErrorMessage =
+    screen.getByText("1원이상의 금액을 입력해 주세요.");
+  return {
+    dateInput,
+    descInput,
+    amountInput,
+    payerInput,
+    addButton,
+    descErrorMessage,
+    payerErrorMessage,
+    amountErrorMessage,
+  };
+};
+
+describe("비용 정산 메인 페이지", () => {
+  /* 비용 추가 컴포넌트 관련 테스트 */
+
+  describe("비용 추가 컴포넌트", () => {
+    test("비용 추가 컴포넌트 렌더링", () => {
+      const { dateInput, descInput, amountInput, payerInput, addButton } =
+        renderComponent();
+
+      expect(dateInput).toBeInTheDocument();
+      expect(descInput).toBeInTheDocument();
+      expect(amountInput).toBeInTheDocument();
+      expect(payerInput).toBeInTheDocument();
+      expect(addButton).toBeInTheDocument();
+    });
+
+    test("비용 추가에 필수적인 값을 입력하지 않고 '추가' 버튼 클릭시, 에러 메시지를 노출한다.", async () => {
+      const {
+        addButton,
+        descErrorMessage,
+        payerErrorMessage,
+        amountErrorMessage,
+      } = renderComponent();
+
+      expect(addButton).toBeInTheDocument();
+      await userEvent.click(addButton);
+
+      expect(descErrorMessage).toHaveAttribute("data-valid", "false");
+      expect(payerErrorMessage).toHaveAttribute("data-valid", "false");
+      expect(amountErrorMessage).toHaveAttribute("data-valid", "false");
+    });
+
+    test('비용 추가에 필수적인 값들을 입력한 후 "추가" 버튼 클릭시, 저장 성공', async () => {
+      const {
+        descInput,
+        amountInput,
+        payerInput,
+        addButton,
+        descErrorMessage,
+        payerErrorMessage,
+        amountErrorMessage,
+      } = renderComponent();
+
+      await userEvent.type(descInput, "장보기");
+      await userEvent.type(amountInput, "30000");
+      await userEvent.selectOptions(payerInput, "영수");
+      await userEvent.click(addButton);
+
+      expect(descErrorMessage).toHaveAttribute("data-valid", "true");
+      expect(amountErrorMessage).toHaveAttribute("data-valid", "true");
+      expect(payerErrorMessage).toHaveAttribute("data-valid", "true");
+    });
+  });
+
+  describe("비용 리스트 컴포넌트", () => {
+    test("비용 리스트 컴포넌트가 렌더링 되는가?", () => {
+      renderComponent();
+      const expenseListComponent = screen.getByTestId("expenseList");
+      expect(expenseListComponent).toBeInTheDocument();
+    });
+  });
+
+  describe("새로운 비용이 입력 되었을 때,", () => {
+    const addNewExpense = async () => {
+      const { dateInput, descInput, payerInput, amountInput, addButton } =
+        renderComponent();
+      await userEvent.type(dateInput, "2022-10-12");
+      await userEvent.type(descInput, "장보기");
+      await userEvent.type(amountInput, "30000");
+      await userEvent.selectOptions(payerInput, "영수");
+      await userEvent.click(addButton);
+    };
+    test("날짜, 내용, 결제자, 금액 데이터가 정산 리스트에 추가 된다.", async () => {
+      await addNewExpense();
+      // 새로운 비용 입력
+      const expenseListComponent = screen.getByTestId("expenseList");
+      const dateValue = within(expenseListComponent).getByText("2022-10-12");
+      expect(dateValue).toBeInTheDocument();
+
+      const descValue = within(expenseListComponent).getByText("장보기");
+      expect(descValue).toBeInTheDocument();
+
+      const amountValue = within(expenseListComponent).getByText("30000원");
+      expect(amountValue).toBeInTheDocument();
+
+      const payerValue = within(expenseListComponent).getByText("영수");
+      expect(payerValue).toBeInTheDocument();
+    });
+  });
+});
