ff0f261a04c63f5e39ebd5355486cabb2c16128a
ziya_312 <manjak312@gmail.com>
2023-02-05 04:03:07 +0900

    Logo 컴포넌트

---
+++ src/shared/ServiceLogo.jsx
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const ServiceLogo = () => {
+  return <StyledLogo>&lt;Dutch Pay&gt;</StyledLogo>;
+};
+
+const StyledLogo = styled.h1`
+  font-weight: 400;
+  font-size: 72px;
+  font-family: "Mabook";
+  text-align: center;
+  color: #ff7a00;
+  -webkit-user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
+`;
