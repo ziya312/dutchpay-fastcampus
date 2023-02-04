7289de3d1740d75ad3abea8b60ef7dad9d6b51d0
ziya_312 <manjak312@gmail.com>
2023-02-05 04:01:43 +0900

    route

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import { RecoilRoot } from "recoil";
 import { AddMembers } from "./components/AddMembers";
 import { CreateGroup } from "./components/CreateGroup";
 import { ExpenseMain } from "./components/ExpenseMain";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
+import { ROUTES } from "./routes";
 
 function App() {
   return (
     <BrowserRouter>
       <RecoilRoot>
         <Routes>
-          <Route path="/" element={<CreateGroup />} />
-          <Route path="/members" element={<AddMembers />} />
-          <Route path="/expense" element={<ExpenseMain />} />
+          <Route path="/" element={<Navigate to={ROUTES.CREATE_GROUP} />} />
+          <Route path={ROUTES.CREATE_GROUP} element={<CreateGroup />} />
+          <Route path={ROUTES.ADD_MEMBERS} element={<AddMembers />} />
+          <Route path={ROUTES.EXPENSE_MAIN} element={<ExpenseMain />} />
         </Routes>
       </RecoilRoot>
     </BrowserRouter>
