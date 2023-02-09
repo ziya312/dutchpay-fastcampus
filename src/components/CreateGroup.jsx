445c1bbaf657f2e4b42233c0d8a75ed7591bb9d0
ziya_312 <manjak312@gmail.com>
2023-02-10 00:15:35 +0900

    div

--- src/components/CreateGroup.jsx
+++ src/components/CreateGroup.jsx
@@ -25,24 +25,26 @@ export const CreateGroup = () => {
     setValidated(true);
   };
   return (
-    <CenteredOverlayForm
-      title="먼저, 더치 페이 할 그룹의 이름을 정해볼까요?"
-      validated={validated}
-      handleSubmit={handleSubmit}
-    >
-      <Form.Group>
-        <Form.Control
-          type="text"
-          required
-          placeholder="ex) 2022 제주도 여행"
-          onChange={(e) => {
-            setGroupName(e.target.value);
-          }}
-        />
-        <Form.Control.Feedback type="invalid" data-valid={validGroupName}>
-          그룹 이름을 입력해 주세요.
-        </Form.Control.Feedback>
-      </Form.Group>
-    </CenteredOverlayForm>
+    <div>
+      <CenteredOverlayForm
+        title="먼저, 더치 페이 할 그룹의 이름을 정해볼까요?"
+        validated={validated}
+        handleSubmit={handleSubmit}
+      >
+        <Form.Group>
+          <Form.Control
+            type="text"
+            required
+            placeholder="ex) 2022 제주도 여행"
+            onChange={(e) => {
+              setGroupName(e.target.value);
+            }}
+          />
+          <Form.Control.Feedback type="invalid" data-valid={validGroupName}>
+            그룹 이름을 입력해 주세요.
+          </Form.Control.Feedback>
+        </Form.Group>
+      </CenteredOverlayForm>
+    </div>
   );
 };
